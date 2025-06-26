cf8735cf16073721b5f2123f9218b0f00fe91b90
llucasgm <lucas.gomesmm19@gmail.com>
2025-06-26 11:52:07 -0300

    feat: Introdução ao JavaScript

---
+++ js/intro.js
@@ -0,0 +1,29 @@
+let idade = 25;
+let nome = "rodrigo";
+let idade2 = "23";
+let resultado1 = idade + Number(idade2);
+console.log(resultado1);
+//concatenação texto + texto ou número
+
+// console.log(idade);
+// idade = 30;
+// console.log(idade);
+// idade = 50;
+// console.log(idade);
+
+// escopo de variáveis
+// chaves abre bloco de código
+// fora do escopo é global
+let idade3 = 30;
+{
+// dentro do escopo é local
+let idade3 = 35;
+console.log(idade3)
+}
+
+
+// console.log("Olá mundo!");
+// console.info("Olá, mundo!");
+// console.warn("Cuidado, mundo!");
+// console.error("Erro, mundo!");
+// console.debug("Depuração, mundo!");
